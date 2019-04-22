ba317ec4a64a4c11a6a9e45a96724f0bd9d9a21c
idanlo <idan2110@gmail.com>
2019-04-22 14:59:15 +0300

    add some routes (home, catalog) and style the navbar (still needs work)

--- web/src/modules/layout/Layout.tsx
+++ web/src/modules/layout/Layout.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
+import Home from '../home/Home';
+import Catalog from '../catalog/Catalog';
 
 class Layout extends React.Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <h1>he</h1>
+                <Switch>
+                    <Route path="/catalog" exact component={Catalog} />
+                    <Route path="/" exact component={Home} />
+                </Switch>
             </div>
         );
     }
--- web/src/modules/navbar/Navbar.tsx
+++ web/src/modules/navbar/Navbar.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import colors from '../../styles/colors';
 
 const Navbar: React.FC = props => {
     return (
-        <div
-            style={{
-                height: 60,
-                background: colors.primary,
-                color: '#000',
-                display: 'flex',
-                flex: 1,
-                flexDirection: 'row',
-                alignItems: 'center'
-            }}
-        >
-            <div>
-                <a href="/" className={styles.link}>
-                    Home
-                </a>
-                <a href="/products/all" className={styles.link}>
-                    Shoes
-                </a>
-            </div>
-        </div>
+        <nav style={{ background: colors.primary }}>
+            <ul className={styles.navbar}>
+                <li className={styles.link}>
+                    <Link to="/">
+                        <img
+                            src="https://cdn.freebiesupply.com/logos/large/2x/nike-4-logo-png-transparent.png"
+                            style={{ width: 60, height: 60 }}
+                        />
+                    </Link>
+                </li>
+                <li className={styles.link}>
+                    <Link to="/catalog">Men</Link>
+                    <Link to="/catalog">Women</Link>
+                    <Link to="/catalog">Kids</Link>
+                </li>
+                <li className={styles.link}>
+                    <input type="text" placeholder="Search" />
+                </li>
+            </ul>
+        </nav>
     );
 };
 
