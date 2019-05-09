05958efa4ccea53178e7e1bc7322ac3cca69692b
idanlo <idan2110@gmail.com>
2019-05-09 19:42:38 +0300

    config jest with ts-jest and create sample tests for the api controller

--- server/test/api.test.ts
+++ server/test/api.test.ts
@@ -1,9 +1,30 @@
-import request from "supertest";
-import app from "../src/app";
+import request from 'supertest';
+import app from '../src/app';
 
-describe("GET /api", () => {
-  it("should return 200 OK", () => {
-    return request(app).get("/api")
-      .expect(200);
-  });
+describe('GET /api', () => {
+    it('/api/product/new - should return 404 if not all body parameters are passed', async () => {
+        const response = await request(app)
+            .post('/api/product/new')
+            .send({
+                name: 'Test'
+            });
+        expect(response.status).toEqual(404);
+    });
+
+    it('/api/product/new - should return error message if not all body parameters are passed', async () => {
+        const response = await request(app)
+            .post('/api/product/new')
+            .send({
+                name: 'Test'
+            });
+        expect(response.body.message).toEqual(
+            'Please send fields: name, price and tags'
+        );
+    });
+
+    it('/api/tags/all - should return an array of tags', async () => {
+        const response = await request(app).get('/api/tags/all');
+        // TODO
+        expect(true).toBeTruthy();
+    });
 });
