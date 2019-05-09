b4f937b3ab6d6bc6eb1aec5cf11c76a4cccfcf57
idanlo <idan2110@gmail.com>
2019-05-09 22:56:10 +0300

    basic grid system and card UI in the catalog component

--- web/src/modules/catalog/Catalog.tsx
+++ web/src/modules/catalog/Catalog.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import { Row, Col, Card as AntCard } from 'antd';
+
+const Card = () => (
+    <div style={{ background: '#ECECEC', padding: '30px' }}>
+        <AntCard title="Card title" bordered>
+            Card content
+        </AntCard>
+    </div>
+);
 
 const Catalog: React.FC = props => (
     <div
@@ -9,7 +18,15 @@ const Catalog: React.FC = props => (
             minHeight: 280
         }}
     >
-        <h1>Catalog</h1>
+        <Row>
+            {Array(50)
+                .fill(1)
+                .map((item, i) => (
+                    <Col span={6} key={i}>
+                        <Card />
+                    </Col>
+                ))}
+        </Row>
     </div>
 );
 
